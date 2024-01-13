830923275a0a08512c9facf798c239fdffc72525
dawidW4 <dawidwesierski4@gmail.com>
2024-01-13 17:15:24 +0100

    dev 1713

--- main.cpp
+++ main.cpp
@@ -357,7 +357,6 @@ void VirtualWorldCycle()
 		if (MessageBox(main_window, message1, "Negocjowana wartość", MB_YESNO) == IDYES) {
 			frame.frame_type = NEGOTIATION_AKCEPT;
 			frame.transfer_value = negotiation_offer;
-			frame.frame_type = MONEY;
 			frame.iID_receiver = negotiation_reciever;
 			G_ID_receiver = negotiation_reciever;
 			G_negotiation_value = negotiation_offer;
@@ -365,7 +364,6 @@ void VirtualWorldCycle()
 		else {
 			frame.frame_type = NEGOTIATION_REFUSE;
 			frame.transfer_value = negotiation_offer;
-			frame.frame_type = MONEY;
 			frame.iID_receiver = negotiation_reciever;
 		}
 		int iRozmiar = multi_send->send((char*)&frame, sizeof(Frame));
