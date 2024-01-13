4ece251374f6bd556b24e9ad9f385be0dee71962
dawidW4 <dawidwesierski4@gmail.com>
2024-01-13 17:17:25 +0100

    dev 1717

--- main.cpp
+++ main.cpp
@@ -205,23 +205,23 @@ DWORD WINAPI ReceiveThreadFunction(void *ptr)
 		case NEGOTIATION_AKCEPT:                       // frame informuj¹ca o przelewie pieniê¿nym lub przekazaniu towaru    
 		{
 			char message1[256];
-			//if (frame.iID == my_vehicle->iID)  // ID pojazdu, ktory otrzymal przelew zgadza siê z moim ID 
-			//{
+			if (frame.iID == my_vehicle->iID)  // ID pojazdu, ktory otrzymal przelew zgadza siê z moim ID 
+			{
 					negotiation_status = AKCEPTED;
 					negotiation_offer = frame.transfer_value;
 					G_ID_receiver = frame.iID;
 					G_negotiation_value = 1 - negotiation_offer;
-			//}
+			}
 			break;
 		}
 
 		case NEGOTIATION_REFUSE:                       // frame informuj¹ca o przelewie pieniê¿nym lub przekazaniu towaru    
 		{
 			char message1[256];
-			//if (frame.iID == my_vehicle->iID)  // ID pojazdu, ktory otrzymal przelew zgadza siê z moim ID 
-			//{
+			if (frame.iID == my_vehicle->iID)  // ID pojazdu, ktory otrzymal przelew zgadza siê z moim ID 
+			{
 					negotiation_status = REFUSED;
-			//}
+			}
 			break;
 		}
 
